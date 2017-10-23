db6a6dcd930c3f3b40b4619d3ec382adbcec7988
Martin Kelnar <mkelnar@redhat.com>
2017-10-23 15:00:20 +0200

    Breadcrumbs added

--- src/pnc-ui-extras/pnc-ui-extras.module.js
+++ src/pnc-ui-extras/pnc-ui-extras.module.js
@@ -1,6 +1,7 @@
 angular.module('pnc-ui-extras', [
   'pnc-ui-extras.templates',
-  'pnc-ui-extras.combobox'
+  'pnc-ui-extras.combobox',
+  'pnc-ui-extras.uiBreadcrumbs'
 ]);
 
 angular.module('pnc-ui-extras.templates', []);
